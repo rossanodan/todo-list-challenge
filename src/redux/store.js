a97b020ffff8810230930d64e24b7ea28b5abf13
Rossano D'Angelo <rossanoa@DFE-MAC-00212.local>
2020-03-18 17:13:13 +0000

    Created middleware to keep track of user actions. Need to work more on that

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,25 @@
-import { createStore } from "redux";
-import reducers from "./reducers/reducers";
+import { createStore, applyMiddleware } from 'redux';
+import reducers from "./reducers";
 
-export default createStore(reducers);
+let tracker;
+
+if (localStorage.getItem('tracker')) {
+  tracker = JSON.parse(localStorage.getItem('tracker'));
+} else {
+  tracker = [];
+}
+
+function logger() {
+  return next => action => {
+    tracker.push(action);
+    localStorage.setItem('tracker', JSON.stringify(tracker));
+    return next(action);
+  }
+}
+
+const store = createStore(
+  reducers,
+  applyMiddleware(logger)
+);
+
+export default store;
