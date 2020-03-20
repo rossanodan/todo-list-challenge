fc0623b5c9ab3c673eb08be4272122205c5c7e3e
Rossano D'Angelo <rossanodan@Rossanos-MacBook-Pro.local>
2020-03-20 15:40:32 +0000

    Refactoring code

--- src/TodosSingleton.js
+++ src/TodosSingleton.js
@@ -1,37 +1,33 @@
 class TodosSingleton {
-    constructor() {
-      this.todos;
-      if (localStorage.getItem('todos')) {
-        this.todos = JSON.parse(localStorage.getItem('todos'));
-      } else {
-        this.todos = [];
-        this.sync();
-      }
-    }
-    add(obj) {
-      this.todos.push(obj);
-      this.sync();
-    }
-    delete(obj) {
-      const id = obj.todo.id;
-      console.log('Removing', id);
-      this.todos = this.todos.filter(todo => todo.id !== id);
-      console.log('New todos', this.todos);
-      // TODO
-      // this.history.remove(obj);
-      this.sync();
-    }
-    get() {
-      return this.todos;
-    }
-    clean() {
+  constructor() {
+    this.todos;
+    if (localStorage.getItem('todos')) {
+      this.todos = JSON.parse(localStorage.getItem('todos'));
+    } else {
       this.todos = [];
       this.sync();
     }
-    sync() {
-      localStorage.setItem('todos', JSON.stringify(this.todos));
-    }
-  };
-  
-  const singleton = new TodosSingleton();
-  export default singleton;
+  }
+  add(obj) {
+    this.todos.push(obj);
+    this.sync();
+  }
+  delete(obj) {
+    const id = obj.todo.id;
+    this.todos = this.todos.filter(todo => todo.id !== id);
+    this.sync();
+  }
+  get() {
+    return this.todos;
+  }
+  clean() {
+    this.todos = [];
+    this.sync();
+  }
+  sync() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+};
+
+const singleton = new TodosSingleton();
+export default singleton;
