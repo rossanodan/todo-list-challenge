568b2c1d1b216535b4d59782b8cbfba455d2a0e7
Rossano D'Angelo <rossanoa@fsb.local>
2020-03-24 12:34:53 +0000

    Working on unit tests

--- spec/App.spec.js
+++ spec/App.spec.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import App from '../src/App';
+
 describe('<App />', () => {
   it('should be initialised without errors', () => {
-    console.log('Hello test!');
-    expect(1).toBe(1);
+    const wrapper = mount(<App />);
+    expect(wrapper).toBeDefined();
+    wrapper.unmount();
   });
 });
